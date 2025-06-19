8b909a0d61ab42797f0dd75b6b7f07d46aa2e8ee
Roberto Blasco~ <roberza2202@gmail.com>
2025-06-19 19:13:44 +0200

    primeros pasos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { useState } from 'react';
+import Task from './components/Task.jsx'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -7,10 +9,32 @@ const App = () => {
     { id: 3, text: 'Hacer ejercicio', completed: false }
   ]);
 
-  
+  const addTask = (text) => {
+    const newTask = {
+      id: tasks.length + 1,
+      text,
+      completed: false 
+    };
+    setTasks([...tasks, newTask]);
+  }
+  //console.log(addTask)
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  const taskComplete = (id) => {
+    setTasks(
+      tasks.map(task =>
+        task.id == id ? {...task, completed: !task.completed} : task
+      )
+    );
+  };
 
   return (
-    <></>
+    <>
+      <h1>Lista de tareas</h1>
+    </>
   );
 };
 
---
+++ src/components/Task.jsx
@@ -0,0 +1,9 @@
+function addTask () {
+    return (
+        <>
+          <h1>Lista de tareas</h1> 
+        </>
+    )
+}
+
+export default addTask
